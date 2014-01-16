533b679ce14dc0b5afa74e850a5ef8d07d6b3151
Richard Taylor <rsjtaylor.github@gmail.com>
2014-01-16 15:26:41 +0000

    simple animating div for baseline app profile

--- static/script/appui/components/simple.js
+++ static/script/appui/components/simple.js
@@ -5,12 +5,22 @@ require.def("sampleapp/appui/components/simple",
         "antie/widgets/label"
     ],
     function (Component, Button, Label) {
-        
+        'use strict';
+        var start = 80, end = 1000, duration = 2000, states = [
+            'start',
+            'out',
+            'end',
+            'back'
+        ];
+
+        var state = 0;
+
         // All components extend Component
         return Component.extend({
-            init: function () {
-                var self, label, button;
 
+            init: function () {
+                var self, label;
+                this._device = this.getCurrentApplication().getDevice();
                 self = this;
                 // It is important to call the constructor of the superclass
                 this._super("simplecomponent");
@@ -24,11 +34,69 @@ require.def("sampleapp/appui/components/simple",
                     self._onBeforeRender(ev);
                 });
 
+                function nextState() {
+                    state += 1;
+                    if (state === states.length) {
+                        state = 0;
+                    }
+                }
+
+
+
+                function tweenOut() {
+                    nextState();
+                    self._device.moveElementTo({
+                        el: self._button.outputElement,
+                        from: {
+                            left: start
+                        },
+                        to: {
+                            left: end
+                        },
+                        onComplete: nextState,
+                        duration: duration
+                    });
+                }
+
+                function tweenBack() {
+                    nextState();
+                    self._device.moveElementTo({
+                        el: self._button.outputElement,
+                        from: {
+                            left: end
+                        },
+                        to: {
+                            left: start
+                        },
+                        onComplete: nextState,
+                        duration: duration
+                    });
+                }
+
+                function tween() {
+                    switch (states[state]) {
+                    case 'start':
+                        tweenOut();
+                        break;
+                    case 'end':
+                        tweenBack();
+                        break;
+                    }
+                }
+
                 // calls Application.ready() the first time the component is shown
                 // the callback removes itself once it's fired to avoid multiple calls.
                 this.addEventListener("aftershow", function appReady() {
                     self.getCurrentApplication().ready();
                     self.removeEventListener('aftershow', appReady);
+                    self._device.moveElementTo({
+                        el: self._button.outputElement,
+                        to: {
+                            left: start
+                        },
+                        skipAnim: true
+                    });
+                    self._button.addEventListener('select', tween);
                 });
             },
 
@@ -36,7 +104,7 @@ require.def("sampleapp/appui/components/simple",
             // if the component is hidden and subsequently reinstated.
             _onBeforeRender: function () {
                 this.appendChildWidget(this._button);
-            } 
+            }
         });
     }
 );
